7718159405b2019acc3723040671aa422b393bc5
ivory <jsivory@itcom666.com>
2024-08-12 22:04:01 +0800

    2024-8-12 22:04

--- packages/goose/craco.config.js
+++ packages/goose/craco.config.js
@@ -1,21 +1,25 @@
+const path = require('path');
+const { getLoader, loaderByName } = require('@craco/craco');
+// git项目react-monorepo
+const packages = [];
+packages.push(path.join(__dirname, '../components'));
+
 module.exports = {
   webpack: {
-    alias: {
-      "@ivory/components": "../components",
+    configure: (webpackConfig, arg) => {
+      const { isFound, match } = getLoader(
+        webpackConfig,
+        loaderByName('babel-loader')
+      );
+      if (isFound) {
+        const include = Array.isArray(match.loader.include)
+          ? match.loader.include
+          : [match.loader.include];
+
+        match.loader.include = include.concat(packages);
+      }
+
+      return webpackConfig;
     },
-    modules: ["node_modules", path.resolve(__dirname, "../")],
-  },
-  babel: {
-    plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["../"],
-          alias: {
-            "@ivory/components": "../components",
-          },
-        },
-      ],
-    ],
   },
 };
